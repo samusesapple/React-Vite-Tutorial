dbebae646183ec74f26d9e0a34b1dfede6be1b07
samusesapple <gwansammy@gmail.com>
2024-11-10 22:01:33 +0900

    🍿 [UI] Asset 이미지 파일 변경 및 Slider 반응형 snap 슬라이더 구현

--- clone-disney/src/components/Slider.tsx
+++ clone-disney/src/components/Slider.tsx
@@ -8,7 +8,6 @@ const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 const Slider = () => {
     const [movieList, setMovieList] = useState<Movie[]>([]);
     const elementRef = useRef<HTMLDivElement>(null);
-    const screenWidth = window.innerWidth;
     const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
 
 
@@ -23,7 +22,8 @@ const Slider = () => {
     }
 
     const sliderRight = (element: HTMLDivElement) => {
-        element.scrollLeft += screenWidth-108;
+        const itemWidth = element.getBoundingClientRect().width;
+        element.scrollLeft += itemWidth;
         setIsButtonDisabled(true);
         setTimeout(() => {
             setIsButtonDisabled(false);
@@ -31,7 +31,8 @@ const Slider = () => {
     }
 
     const sliderLeft = (element: HTMLDivElement) => {
-        element.scrollLeft -= screenWidth-108;
+        const itemWidth = element.getBoundingClientRect().width;
+        element.scrollLeft -= itemWidth;
         setIsButtonDisabled(true);
         setTimeout(() => {
             setIsButtonDisabled(false);
@@ -48,10 +49,10 @@ const Slider = () => {
                 className={`hidden md:block text-white text-[30px] absolute right-0 mr-8 cursor-pointer ${isButtonDisabled ? 'opacity-30' : ''}`}
                 onClick={() => elementRef.current && sliderRight(elementRef.current)} 
             />
-            <div className="flex overflow-x-auto scrollbar-none scroll-smooth w-full px-16 py-4" ref={elementRef}>
+            <div className="flex overflow-x-auto md:overflow-x-hidden scrollbar-none scroll-smooth snap-x snap-mandatory w-full px-16 py-4" ref={elementRef}>
                 {movieList.map((movie, index) => (
                     <img
-                        className="min-w-full md:h-[310px] object-cover object-top mr-5 rounded-md hover:border-[4px] hover:border-gray-400 transition-all duration-100 ease-in"
+                        className="min-w-full md:h-[310px] object-cover object-center mr-5 rounded-md hover:border-[4px] hover:border-gray-400 transition-all duration-100 ease-in snap-center shadow-[8px_10px_10px_0px_rgba(0,0,0,0.4)]"
                         src={`${IMAGE_BASE_URL}${movie.backdrop_path}`}
                         key={index}
                     />  
